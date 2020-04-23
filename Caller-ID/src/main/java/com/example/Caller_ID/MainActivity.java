42aa2a7e0b53530b97a8fd975d341e94da27d472
Олег Максименко <oleg.maksimenko@me.com>
2020-04-23 13:53:09 +0300

    local database

--- Caller-ID/src/main/java/com/example/Caller_ID/MainActivity.java
+++ Caller-ID/src/main/java/com/example/Caller_ID/MainActivity.java
@@ -1,30 +1,85 @@
 package com.example.Caller_ID;
 
+import android.app.DownloadManager;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import android.content.Context;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper mDatabaseHelper;
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
+    StorageReference ref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //mDatabaseHelper = new DatabaseHelper(this);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_call_log,
                 R.id.navigation_check_number, R.id.navigation_spam_protection, R.id.navigation_settings)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+        //uncommeny this to create local database and add record
+        //String testPhone="+7-495-999-99-99";
+        //Boolean isSpam=false;
+        //mDatabaseHelper.addRecord(testPhone, isSpam);
+        download();
+    }
+
+    public void download() {
+        storageReference = firebaseStorage.getInstance().getReference();
+        ref = storageReference.child("phoneTable");
+
+        ref.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                String url = uri.toString();
+                downloadFiles(MainActivity.this,
+                        "phoneTable",
+                        "",
+                        "/data/data/com.example.myapplication/databases/",
+                         url);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+    }
+
+    public void downloadFiles(Context context,
+                              String fileName,
+                              String fileExtension,
+                              String destinationDirectory,
+                              String url) {
+        DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(url);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalFilesDir(context, destinationDirectory, fileName+fileExtension);
+
+        downloadManager.enqueue(request);
     }
 
 }
