6afa2e8d448c5dda7cdd402847f3f2398ec3968e
Punk_Off <pankov.kirill1997@yandex.ru>
2020-05-25 18:32:41 +0300

    Add Thread and Handler

--- Caller-ID/src/main/java/com/example/Caller_ID/ui/callLog/CallLogFragment.java
+++ Caller-ID/src/main/java/com/example/Caller_ID/ui/callLog/CallLogFragment.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
