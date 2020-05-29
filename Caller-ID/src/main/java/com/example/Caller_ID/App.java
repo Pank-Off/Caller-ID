b6a0153f3091ba6e55f30590acc86fd09dab63bd
Олег Максименко <oleg.maksimenko@me.com>
2020-05-29 14:06:23 +0300

    implemented date check

--- Caller-ID/src/main/java/com/example/Caller_ID/App.java
+++ Caller-ID/src/main/java/com/example/Caller_ID/App.java
@@ -1,6 +1,12 @@
 package com.example.Caller_ID;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
 
 public class App extends Application {
 
@@ -10,10 +16,13 @@ public class App extends Application {
     // база данных
     private DatabaseHelper db;
 
+    SharedPreferences sPref;
+
     // Так получаем объект приложения
     public static App getInstance() {
         return instance;
     }
+    public static final String APP_PREFERENCES = "callerIDPreferences";
 
     @Override
     public void onCreate() {
@@ -25,6 +34,37 @@ public class App extends Application {
 
         fireBaseWorker = new FireBaseWorker(getApplicationContext());
         fireBaseWorker.download();
+
+        DateFormat df = new SimpleDateFormat("dd/MM/yy");
+        String currentDate = df.format(new Date());
+        String dateTimeKey = "lastUpdateDate";
+
+        SharedPreferences sharedPreferences = getSharedPreferences(APP_PREFERENCES, 0);
+
+        if (sharedPreferences.getBoolean("firstRun", true)) {
+            sharedPreferences.edit().putBoolean("firstRun", false).commit();
+            fireBaseWorker = new FireBaseWorker(getApplicationContext());
+            fireBaseWorker.download();
+
+            sharedPreferences.edit().putString(dateTimeKey, currentDate);
+            sharedPreferences.edit().apply();
+        }
+        else {
+            String lastUpdateDate = sharedPreferences.getString(dateTimeKey, currentDate);
+            Date lastDate;
+            try {
+                lastDate = df.parse(lastUpdateDate);
+                if (!currentDate.equals(lastUpdateDate)) {
+                    fireBaseWorker = new FireBaseWorker(getApplicationContext());
+                    fireBaseWorker.download();
+
+                    sharedPreferences.edit().putString(dateTimeKey, currentDate);
+                    sharedPreferences.edit().apply();
+                }
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public DatabaseHelper getDataBase() {
